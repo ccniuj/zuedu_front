b4733caabb216bf22b41e74c2689d726fbabc3b6
Juin Chiu <davidjuin0519@gmail.com>
2016-07-10 16:02:43 +0800

    add .js extension

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './index'
+    './index.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
