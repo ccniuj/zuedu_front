6c02fa9668ac152f14591f79c4ca9c856e7cd8f6
Juin Chiu <davidjuin0519@gmail.com>
2016-07-10 22:33:53 +0800

    add favicon

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('./webpack.config')
-
-var app = new (require('express'))()
+var express = require('express')
+var path = require('path')
+var app = new (express)()
 var port = 3001
 
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.get("*", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
