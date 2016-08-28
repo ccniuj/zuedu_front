e4098fd0d0d31de6f19e3809d2f32386b323d783
Juin Chiu <davidjuin0519@gmail.com>
2016-08-28 14:10:02 +0800

    add content

--- containers/CartContainer.js
+++ containers/CartContainer.js
@@ -65,7 +65,13 @@ class CartContainer extends Component {
       <div className='container' style={style}>
         <center><h3>購物車</h3></center>
         <input type='submit' className='btn btn-info' onClick={() => addToCart(parseInt(Object.keys(products)[0])).then(() => getCart(), err => console.log(err))} value='新增' />
-        <br/>
+        <p>
+          請輸入學生及聯絡人資料，並選擇這位學生要參加的梯次。
+          若要團報，請先填寫一位學生的資料，點選“＋”按鈕；再填寫另一位學生的資料。
+          “確定” 所有學生都填完以後，才點 “一次” 右邊的 確認送出，進行繳費 按鈕。
+          若資料輸入途中不慎將網頁關閉，資料將會自動儲存到您的個人帳戶中，可以在個人後台內點選並繼續進行報名。
+          紅色星號為必填欄位
+        </p>
         { applicants.map(applicant => 
             <ApplicantForm ref={`applicant_${applicant.id}`} 
                            key={applicant.id}
