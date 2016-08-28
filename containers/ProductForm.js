0de6a02efb36497f999e4255622798a63fa4a2bc
Juin Chiu <davidjuin0519@gmail.com>
2016-08-28 13:34:34 +0800

    move fb share link load code into react component

--- containers/ProductForm.js
+++ containers/ProductForm.js
@@ -45,6 +45,14 @@ class ProductForm extends Component {
     }
 
     LineIt.loadButton()
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1535205933369498";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }
 
   render() {
@@ -64,6 +72,7 @@ class ProductForm extends Component {
              style={{display: 'none'}} 
              data-type="share-b" 
              data-lang="zh-Hant" />
+        <div id="fb-root"></div>
         <div className="fb-share-button" 
              data-href={`${redirect_url}`} 
              data-layout="button" 
