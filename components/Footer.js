71e9fd3fae829829cbf0cd100f5f8e33e3975906
daniel840829 <daniel840829@yahoo.com.tw>
2017-03-28 15:08:35 +0800

    landing page 95% icon is done ,last thing is the home-block

--- components/Footer.js
+++ components/Footer.js
@@ -11,27 +11,30 @@ export default class Footer extends Component {
     return (
       <footer className='footer-container'>
       <div className="row">
-        <div className="col-md-5 col-xs-3 footerlogo ">
+        <div className="col-md-5 col-xs-5 footerlogo ">
           <div className="row ">
           <div className='col-md-6 footer-logo'>
-            <img className='col-md-10 col-md-offset-1 col-xs-12' src='/images/logo_footer.png' />
+            <img className='col-md-10 col-md-offset-1 col-xs-6' src='/images/logo_footer.png' />
           </div>
-          <div className='col-md-6 col-xs-12 footer-slogan'>
+          <div className='col-md-6 col-xs-6 footer-slogan'>
             <br/>
             <h5>Where creativity meets</h5><h5>next generation.</h5>
           </div>
         </div>
       </div>
-      <div className="col-md-7 col-xs-8">
+      <div className="col-md-7 col-xs-7">
         <div className="col-md-4 col-xs-6 footer-icon">
           <img className="col-xs-6 "src="images/facebook.png" alt=""/>
           <img className="col-xs-6"src="images/line.png" alt=""/>
         </div>
-        <div className='col-md-8 col-xs-2 col-xs-offset-1 footer-detail'>
+        <div className='col-md-8 col-md-offset-0 col-xs-6 col-xs-offset-0 footer-detail'>
+        <h5>
           信箱：zubat.nthu@gmail.com<br/>
           LINE：@ebr1368g<br/>
           客服：0975-467-941<br/>
           辦公室地址:<br/>
+        </h5>
+          
         </div>
         </div>
       </div>
